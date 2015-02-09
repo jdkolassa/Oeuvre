a818ecda5b602a07ac695b87ddacaacde9cf6fd2
Damien Stanton <info@damienkstanton.com>
2015-02-09 11:20:43 -0500

    Fixed bug 4

--- gruntfile.js
+++ gruntfile.js
@@ -2,17 +2,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     nodewebkit: {
       options: {
-          platforms: ['win','osx', 'linux'],
-          buildDir: './webkitbuilds', // Where the build version of my node-webkit app is saved
+      platforms: ['win', 'osx', 'linux'],
+      buildDir: './dist',
       },
-      src: ['./example/public/**/*'] // Your node-webkit app
+      src: ['./**/*']
     },
-  });
+});
 
-  // Load node-webkit generator plugin
+// Load and register generator and task
+grunt.loadNpmTasks('grunt-node-webkit-builder');
+grunt.registerTask('default', ['nodewebkit']);
 
-  grunt.loadNpmTasks('grunt-node-webkit-builder');
-
-  // Register nodewebkit task
-  grunt.registerTask('default', ['nodewebkit']);
 };
